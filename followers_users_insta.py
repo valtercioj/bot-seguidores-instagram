75e8c3e8d7ca7c0210eb76af896f79add1cfae9c
valtercioj <juniorvaltercio2000@gmail.com>
2020-08-04 19:12:08 -0300

    bot_instragram com python

---
+++ followers_users_insta.py
@@ -0,0 +1,63 @@
+from selenium import webdriver
+import time
+import PySimpleGUI as sg
+
+
+
+
+	
+def tela():
+	sg.change_look_and_feel('DarkPurple2')
+	layout = [
+		[sg.Text('Digite seu email',size=(12,0)),sg.Input(size=(18,0),key='email')],
+		[sg.Text('Digite sua senha',size=(12,0)),sg.Input(size=(18,0),key='password')],
+	    [sg.Text('Usuario para extrair seguidores',size=(12,0)),sg.Input(size=(18,0),key='user')],
+	    [sg.Text('Numeros de seguidores que deseja',size=(12,0)),sg.Input(size=(18,0),key='n_likes')],
+	    [sg.Button('enviar dados')],
+	    [sg.Image(filename=r'bot_insta.png')]
+	]
+	janela = sg.Window('Bot Instagram').layout(layout)
+	Button, values = janela.Read()
+	email = values['email']
+	password = values['password']
+	user = values['user']
+	n_likes = int(values['n_likes'])
+	lista_parametros = []
+	lista_parametros.append(email)
+	lista_parametros.append(password)
+	lista_parametros.append(user)
+	lista_parametros.append(n_likes)
+	return lista_parametros
+
+
+parametro = tela()
+def followers(): # funcao para seguir pessoas
+	
+	time.sleep(3)
+	driver = webdriver.Chrome()
+	url = 'https://www.instagram.com/'
+	driver.get(url)
+	time.sleep(2)
+	username = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input')
+	username.send_keys(parametro[0])
+	time.sleep(3)
+	passw = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input')
+	passw.send_keys(parametro[1])
+	time.sleep(3)
+	enviar = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button').click()
+	time.sleep(10)
+	driver.get(url+parametro[2])
+	time.sleep(2)
+	driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a').click()
+	time.sleep(2)
+	n_likes = parametro[3]
+	for n in range(1,n_likes+1):
+		try:
+			follows = driver.find_element_by_xpath(f'/html/body/div[4]/div/div/div[2]/ul/div/li[{n}]/div/div[3]/button').click()
+			users = driver.find_element_by_xpath(f'/html/body/div[4]/div/div/div[2]/ul/div/li[{n}]/div/div[2]/div[2]/div')
+			print(f'Seguindo {users.text}')
+			time.sleep(1)
+
+		except:
+			continue
+followers()
